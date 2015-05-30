5d0cf375829cd33abda48472f8edc74ffd58eaa7
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-05-30 19:28:48 +0200

    update style

--- site/blueprints/home.php
+++ site/blueprints/home.php
@@ -3,10 +3,15 @@
 title: Home
 pages: false
 fields:
-  title:
-    label: Title
-    type:  text
-  text:
-    label: Text
-    type:  textarea
-    size:  large
+	title:
+		label: Title
+		type:  text
+	text:
+		label: Text
+		type:  textarea
+		size:  large
+	cover:
+		label: Image de fond
+		type: select
+		options: images
+		width: 1/4
