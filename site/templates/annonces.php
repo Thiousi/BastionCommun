3a36854e1ddbd1eb087c0777f27277c407b8299a
Ventricule <pierretandille@gmail.com>
2015-06-08 14:15:26 +0200

    liste annonces

--- site/templates/annonces.php
+++ site/templates/annonces.php
@@ -5,6 +5,10 @@
     <div class="text">
       <h1><?php echo $page->title()->html() ?></h1>
       <?php echo $page->text()->kirbytext() ?>
+      <?php foreach( $page->children() as $annonce ) : ?>
+        <h2><a href="<?php echo $annonce->url() ?>"><?php echo $annonce->title() ?></a></h2>
+      <?php endforeach; ?>
+      <button><a href="">Publier une annonce</a></button>
     </div>
 
   </main>
