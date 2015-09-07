e9ece215c4e04f4061bb9253683fa58163c97f3d
Ventricule <pierretandille@gmail.com>
2015-09-08 01:38:09 +0200

    couleur

--- site/snippets/annonce-editor.php
+++ site/snippets/annonce-editor.php
@@ -2,19 +2,17 @@
 <form action="<?php echo page('smart-submit')->url().'?handler=edit' ?>" data-response-div="annonce" method="post" class="smart-submit">
 
 	<div class="viewOnly">
-		<button class="btn btn-primary btn-lg button editButton" type="button"><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span>  Modifier</button> 
+		<button class="btn btn-success btn-lg button editButton" type="button"><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span>  Modifier</button> 
 	</div>
 
 	<div class="btn-group editOnly" role="group">
-		<div class="btn-group" role="group">
+
 			<button type="submit" name="submit" class="btn btn-lg btn-primary submitButton"><span class='glyphicon glyphicon-ok' aria-hidden='true'></span> Enregistrer</button> 
-		</div>
-		<div class="btn-group" role="group">
+
 			<button type="button" name="Cancel" class="btn btn-lg btn-info cancelButton"><span class='glyphicon glyphicon-remove' aria-hidden='true'></span> Annuler</button> 
-		</div>
-		<div class="btn-group" role="group">
+
 			<button type="button" class="btn btn-lg btn-danger col-xs-12" data-toggle="modal" data-target="#modal-delete"><span class='glyphicon glyphicon-trash' aria-hidden='true'></button>	
-		</div>
+
 	</div>
 	
 	<!-- HIDDEN FORM -->
