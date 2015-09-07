c1083945147991875aeec2372197b2ad80cf0bb4
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-09-07 18:56:32 +0200

    2 columns website
    go megablock, go!

--- site/snippets/annonce-editor.php
+++ site/snippets/annonce-editor.php
@@ -2,18 +2,20 @@
 <form action="<?php echo page('smart-submit')->url().'?handler=edit' ?>" data-response-div="annonce" method="post" class="smart-submit">
 
 	<div class="viewOnly">
-		<button class="btn col-xs-12 btn-primary btn-lg button editButton" data-toggle="button"><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span>  Modifier</button> 
+		<button class="btn btn-primary btn-lg button editButton" data-toggle="button"><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span>  Modifier</button> 
 	</div>
 
-	<div class="btn-group btn-group-justified" role="group">
-		<div class="btn-group editOnly" role="group">
-			<button type="submit" name="submit" class="btn btn-success submitButton"><span class='glyphicon glyphicon-ok' aria-hidden='true'></span> Enregistrer</button> 
+	<div class="btn-group" role="group">
+		<div class="btn-group editOnly " role="group">
+			<button type="submit" name="submit" class="btn btn-lg btn-success submitButton"><span class='glyphicon glyphicon-ok' aria-hidden='true'></span> Enregistrer</button> 
 		</div>
 		<div class="btn-group editOnly" role="group">
-			<button type="submit" name="submit" class="btn btn-primary cancelButton"><span class='glyphicon glyphicon-remove' aria-hidden='true'></span> Annuler</button> 
+			<button type="submit" name="submit" class="btn btn-lg btn-primary cancelButton"><span class='glyphicon glyphicon-remove' aria-hidden='true'></span> Annuler</button> 
+		</div>
+		<div class="btn-group editOnly" role="group">
+			<button type="button" class=" btn btn-lg btn-danger col-xs-12" data-toggle="modal" data-target="#modal-delete"><span class='glyphicon glyphicon-trash' aria-hidden='true'></button>	
 		</div>
 	</div>
-	<button type="button" class=" editOnly btn btn-danger col-xs-12" data-toggle="modal" data-target="#modal-delete"><span class='glyphicon glyphicon-trash' aria-hidden='true'> Supprimer l'annonce</button>	
 	
 	<!-- HIDDEN FORM -->
 	<div id="hiddenForm" class="hidden">
