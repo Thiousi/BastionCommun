75134d5f2b61fb91820854b7adaa4209d4209bb4
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-09-08 02:50:09 +0200

    site dispo hors connexion

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,14 +11,14 @@
 
 	<?php echo css(array(
 		'assets/css/datepicker.css',
-    	'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
+    	'assets/css/bootstrap.min.css',
 		'assets/css/bootstrap-select.min.css',
 		'assets/css/bootstrap-toggle.min.css',
-		'https://cdnjs.cloudflare.com/ajax/libs/medium-editor/4.10.1/medium-editor.min.css',
-		'https://cdnjs.cloudflare.com/ajax/libs/medium-editor/4.10.2/css/themes/flat.min.css',
-	    '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
+		'assets/css/medium-editor.min.css',
+		'assets/css/flat.min.css',
+	    'assets/css/font-awesome.min.css',
 		'assets/css/entypo.css',
-	    'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.0.7/css/swiper.min.css',
+	    'assets/css/swiper.min.css',
     	'assets/css/main.css',
     	'assets/css/perfect-scrollbar.css'
 	)) ?>
@@ -27,9 +27,9 @@
 		'assets/js/jquery-1.11.2.min.js',
 		'assets/js/smart-submit.js',
 		'assets/js/script.js',
-		'https://cdnjs.cloudflare.com/ajax/libs/medium-editor/4.10.1/medium-editor.min.js',
-    	'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.0.7/js/swiper.jquery.min.js',
-    	'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js',
+		'assets/js/medium-editor.min.js',
+    	'assets/js/swiper.jquery.min.js',
+    	'assets/js/bootstrap.min.js',
     	'http://maps.google.com/maps/api/js?sensor=false&libraries=places',
 		'assets/js/me-markdown.standalone.min.js',
     	'assets/js/jquery.ui.widget.js',
