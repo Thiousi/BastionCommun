176f7f88d486496ac465d87a3b040af466b94c2d
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-11-27 18:36:23 +0100

    du mieux

--- site/snippets/homepage.php
+++ site/snippets/homepage.php
@@ -2,8 +2,13 @@
 
 <div id="megabloc" class="accueil large">
 	<div id="column-content" class="column">
-		<div id="cover"></div>
+		<div id="cover" style="background:url(<?php echo page('home')->images()->first()->url() ?>)">
+			<div id="textAccueil" class="column">
+				<?php echo page('home')->text()->kirbytext() ?>
+			</div>
+		</div>
 		<div id="content" class="col-xs-12">
+			<h2 class="subtitle">Dernières annonces publiées :</h2>		
 			<div id="annonces-wrapper">
 				<?php $results = page('annonces')->children()->sortBy('modified', 'desc'); ?>
 				<?php snippet('liste-annonces', array ('results'=>$results)); ?>
