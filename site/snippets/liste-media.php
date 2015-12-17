e6fdb262a588687ade9e192bf80689527ba5071f
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-12-17 11:27:45 +0100

    menu fix

--- site/snippets/liste-media.php
+++ site/snippets/liste-media.php
@@ -8,7 +8,7 @@ foreach ($files as $file) :
     		<?php if ($file->type() == 'image') : ?>
     			<div class="swiper-image" style="background-image:url('<?php echo $file->url() ?>')"></div> 
     		<?php elseif ($file->type() != 'code' && $file->extension() == 'md'): ?>
-    			<div class="swiper-image video-file" style="background-color:#DEDEDE;"><span class="glyphicon glyphicon-play"></span><p><?php echo $file->read() ?></p></div> 
+    			<div class="swiper-image video-file" style="background-color:#DEDEDE;"><span class="glyphicon glyphicon-film"></span><p><?php echo $file->read() ?></p></div> 
     		<?php endif; ?>
     		<div class="media-caption"><input value="<?php echo $file->caption() ?>"></div>
     		<div class="media-delete btn btn-danger"><i class="glyphicon glyphicon-trash"></i></div>
